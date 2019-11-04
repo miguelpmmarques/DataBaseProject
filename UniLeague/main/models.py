7ba76868e73f74c42be86729ad3bc324aa3aa1fb
paulotomas14 <paulotomas14@gamil.com>
2019-11-04 01:48:19 +0000

    Positions, Team, tactic models

--- UniLeague/main/models.py
+++ UniLeague/main/models.py
@@ -25,3 +25,47 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Tactic(models.Model):
+    id = models.BigIntegerField(primary_key=True, null=False, blank=False)
+    nrStrikers = models.IntegerField(null=False, blank=False)
+    nrForwards = models.IntegerField(null=False, blank=False)
+    nrMidfielders = models.IntegerField(null=False, blank=False)
+    nrDefenders = models.IntegerField(null=False, blank=False)
+    nrGoalies = models.IntegerField(null=False, blank=False)
+
+    class Meta:
+        db_table = "Tactic"
+        verbose_name = "Tatica"
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=512, unique=True, null=False)
+    start = models.BooleanField()
+
+    class Meta:
+        db_table = "Position"
+        verbose_name = "Posicao"
+        verbose_name_plural = "Posicoes"
+        ordering = ["-name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=512, null=False)
+    numberPlayers = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = "Team"
+        verbose_name = "Equipa"
+        verbose_name_plural = "Equipas"
+        ordering = ["-name"]
+
+    def __str__(self):
+        return self.name
