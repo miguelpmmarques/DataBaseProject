39f4719b7cb9bc3d8a97df1eca7af764d84a05c8
RodrigoRafael <rodrigorafael2605@gmail.com>
2019-11-19 21:46:58 +0000

    save

--- UniLeague/main/static/js/profile.js
+++ UniLeague/main/static/js/profile.js
@@ -4,3 +4,22 @@ if (document.readyState === "complete" ||
 } else {
     document.addEventListener("DOMContentLoaded", main);
 }
+
+function main() {
+  var btns = document.getElementsByClassName("btn");
+  console.log(btns)
+
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', myFunction);
+  }
+
+  function myFunction(e){
+    $.ajax({url: "",
+          type: 'PATCH', timeout: 3000, data: { 'team': this.id }//, processData:false, contentType = 'application/json'
+      })
+      .fail(function(){
+          alert('Error viewing this team.');
+      });
+
+  }
+}
